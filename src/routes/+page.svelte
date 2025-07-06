2def675427d61b0bc765350acf59512b2bb3f87e
lanye74 <layne.ogden727@gmail.com>
2025-07-06 19:02:38 -0400

    emphasized key words

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome freaks and faggots</h1>
+<style>
+	span {
+		color: oklch(0.7 0.1 0);
+		animation: gay 3s infinite;
+	}
+</style>
+
+<h1>Welcome freaks and <span>faggots</span></h1>
 <p>please leave.</p>
