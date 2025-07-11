0328cc0c48c9de04b6fb6f4b46a78d67d31ddcd0
lanye74 <layne.ogden727@gmail.com>
2025-07-11 11:22:07 -0400

    chore: silence devtools complaints

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import {defineConfig} from "vite";
 
+import pluginDevTools from "vite-plugin-devtools-json";
 import {sveltekit as pluginSvelteKit} from "@sveltejs/kit/vite";
 
 
 
 export default defineConfig(_args => {
 	return {
-		plugins: [pluginSvelteKit()]
+		plugins: [
+			pluginSvelteKit(),
+			pluginDevTools()
+		]
 	}
 });
