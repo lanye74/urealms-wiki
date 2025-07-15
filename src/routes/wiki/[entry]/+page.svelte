79430e619ad8261fc150778db27a4f0963d4375e
lanye74 <layne.ogden727@gmail.com>
2025-07-15 11:24:08 -0400

    running in circles; make pages more reliable

--- src/routes/wiki/[entry]/+page.svelte
+++ src/routes/wiki/[entry]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	const {data} = $props();
+	let {data} = $props();
 </script>
 
 
--- src/routes/wiki/[entry]/+page.ts
+++ src/routes/wiki/[entry]/+page.ts
@@ -1,4 +1,4 @@
-import {clientPageRegistry} from "$utils/pageRegistry.js";
+import type {Component} from "svelte";
 
 
 
@@ -6,11 +6,20 @@ export async function load({params}) {
 	const pageRequest = params.entry;
 	const pagePath = `/src/lib/wiki/${pageRequest}.svelte`;
 
-	const pageModule = clientPageRegistry[pagePath]();
+
+	// TODO: this isn't how i would like it to be,
+	// but it's more reliable than it was before. so whatever
+	const clientPageRegistry = import.meta.glob("/src/lib/wiki/*.svelte", {
+		eager: false,
+		import: "default"
+	}) as Record<string, () => Promise<Component>>;
+
+
+	const wikiPagePromise = clientPageRegistry[pagePath];
 
 
 
 	return {
-		page: pageModule
+		page: wikiPagePromise()
 	};
 }
