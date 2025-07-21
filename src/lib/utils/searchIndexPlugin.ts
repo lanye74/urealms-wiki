6189d6afbbaffdb4c2e52fe967d097720268cf86
lanye74 <layne.ogden727@gmail.com>
2025-07-21 09:38:14 -0400

    use `filter`; read documentation more
    hooray !

--- src/lib/utils/searchIndexPlugin.ts
+++ src/lib/utils/searchIndexPlugin.ts
@@ -26,18 +26,32 @@ export default function searchIndexPlugin(options?: SearchIndexPluginOptions): P
 
 
 
-		// TODO: start parsing files into their content!
+		// although it feels weird to hook into "transformation" just to
+		// read a file's content, this step comes right after the file is `load()`ed
+		// ...where else to do it? :-)
 		transform: {
 			order: "pre",
 
+			filter: {
+				// TODO: double check: is the `code` field useful?
+				// https://rollupjs.org/plugin-development/#build-hooks
+				// i don't believe it is—it seems to just be a secondary filter so that
+				// matched files must also pass the regex. but i don't believe that it
+				// actually trims the `src` field passed to the handler
+				id: wikiPageRegex
+			},
+
 			handler: (fileSource, filePath) => {
-				const match = filePath.match(wikiPageRegex)?.[0];
-
-				if(!match) return;
-
+				// because of the filter, this should always exist
+				const match = filePath.match(wikiPageRegex)![0];
 				const fileName = match.split("/").pop()!;
 
+				// console.log(`hook ran for ${fileName}`);
 				pageSourceIndex[fileName] = fileSource;
+
+				// TODO: start parsing files into their content!
+
+				// console.log(`file name contents: ${fileSource}`);
 			}
 		},
 
